e0daa196ff09b1112233383ea8e6b030b71f5d0e
Jordi <jordi95e@gmail.com>
2021-12-09 13:26:02 +0100

    Pruebas pulsaciones encima de carta con OnMouseDown + BoxColider

--- CardScript.cs
+++ CardScript.cs
@@ -12,10 +12,16 @@ public class CardScript : MonoBehaviour
     {
 
     }
+    
+    // Click encima de la carta ( el GameObject tiene que tener un BoxColider )
+    // Dudas de como funciona, si creamos la funcion y no la estamos asignando ni llamando a nada como puede funcionar?
+    private void OnMouseDown()
+    {
+        Debug.Log("Click en carta " + name);
+    }
 
     // Update is called once per frame
     void Update()
     {
         
     }
-}
